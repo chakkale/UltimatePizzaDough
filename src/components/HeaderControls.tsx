d7c80ff7314909075842030eb878a50812c70dfb
Doğukan Atlıhan <dogukan.atlihan@udogames.com>
2025-02-28 23:16:19 +0300

    Added GitHubButton component and integrated into HeaderControls
    A new GitHubButton component has been created, which is a styled anchor tag that takes in theme and repoUrl as props. This button changes its appearance based on the provided theme (dark or light) and directs to the given repository URL when clicked. The button also includes an SVG icon for visual representation of GitHub.
    
    This newly created GitHubButton has been integrated into the existing HeaderControls component. A ButtonGroup styled-component was added to group AddToHomeButton and GitHubButton together, ensuring they align properly within the header controls. The repository URL is hardcoded within HeaderControls for now.

--- src/components/HeaderControls.tsx
+++ src/components/HeaderControls.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import ThemeToggle from './ThemeToggle';
 import AddToHomeButton from './AddToHomeButton';
+import GitHubButton from './GitHubButton';
 import { useTheme } from '../context/ThemeContext';
 
 const ControlsContainer = styled.div`
@@ -12,13 +13,22 @@ const ControlsContainer = styled.div`
   margin-top: 1rem;
 `;
 
+const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const HeaderControls: React.FC = () => {
   const { theme } = useTheme();
+  const repoUrl = 'https://github.com/chakkale/UltimatePizzaDough';
   
   return (
     <ControlsContainer>
       <ThemeToggle />
-      <AddToHomeButton theme={theme} />
+      <ButtonGroup>
+        <AddToHomeButton theme={theme} />
+        <GitHubButton theme={theme} repoUrl={repoUrl} />
+      </ButtonGroup>
     </ControlsContainer>
   );
 };
